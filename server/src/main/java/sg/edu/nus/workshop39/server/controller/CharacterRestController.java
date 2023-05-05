36440b728d7ae5e463fee3edde17a29d56e714f2
kenken64 <bunnyppl@hotmail.com>
2023-05-05 14:20:00 +0800

    upd

---
+++ server/src/main/java/sg/edu/nus/workshop39/server/controller/CharacterRestController.java
@@ -0,0 +1,96 @@
+package sg.edu.nus.workshop39.server.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
+import sg.edu.nus.workshop39.server.model.Comment;
+import sg.edu.nus.workshop39.server.model.MarvelCharacter;
+import sg.edu.nus.workshop39.server.services.CharacterService;
+
+@RestController
+@RequestMapping(path="/api/characters", consumes=MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+public class CharacterRestController {
+    
+    @Autowired
+    private CharacterService charsvc;
+
+    @GetMapping
+    public ResponseEntity getCharacters(
+        @RequestParam(required=true) String charName,
+        @RequestParam(required=true) Integer limit,
+        @RequestParam(required=true) Integer offset
+        
+    ){
+        JsonArray result = null;
+        Optional<List<MarvelCharacter>> arr = this.charsvc.getCharacters(charName, limit, offset);
+        List<MarvelCharacter> aa = arr.get();
+        JsonArrayBuilder arrBld = Json.createArrayBuilder();
+        for(MarvelCharacter c: aa)
+            arrBld.add(c.toJSON());
+        result = arrBld.build();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(result.toString());
+    }
+
+    @GetMapping(path="/{charId}")
+    public ResponseEntity<String> getCharacterDetails(
+        @PathVariable(required=true) String charId
+    ) throws IOException{
+        MarvelCharacter c = this.charsvc.getCharacterDetails(charId);
+        JsonObjectBuilder bld = Json.createObjectBuilder()
+            .add("details", c.toJSON());
+        JsonObject result = bld.build();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(result.toString());
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> saveCharacterComment(
+        @RequestBody Comment comment, 
+        @PathVariable(required=true) String charId
+    ){
+        comment.setCharId(charId);
+        Comment r= this.charsvc.insertComment(comment);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(r.toString());
+    }
+
+    @GetMapping(path="/comments/{charId}")
+    public ResponseEntity<String> getCharComments(@PathVariable(required=true) String charId){
+        List<Comment> aa=  this.charsvc.getAllComments(charId);
+        JsonArrayBuilder bld = Json.createArrayBuilder();
+        for(Comment a : aa)
+            bld.add(a.toJSON());
+        JsonArray result =bld.build();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(result.toString());
+    }
+}
--- server/src/main/java/sg/edu/nus/workshop39/server/services/CharacterService.java
+++ server/src/main/java/sg/edu/nus/workshop39/server/services/CharacterService.java
@@ -1,5 +1,64 @@
 package sg.edu.nus.workshop39.server.services;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.joda.time.Instant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import sg.edu.nus.workshop39.server.model.Comment;
+import sg.edu.nus.workshop39.server.model.MarvelCharacter;
+import sg.edu.nus.workshop39.server.repository.CharCommentRepository;
+
+@Service
 public class CharacterService {
-    
+    @Autowired
+    private MarvelApiService marvelApisvc;
+
+    @Autowired
+    private CharCommentRepository charCommentRepo;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+
+    public Optional<List<MarvelCharacter>> getCharacters(String characterName,
+        Integer limit, Integer offset){
+        return this.marvelApisvc.getCharacters(
+            characterName, limit, offset);    
+    }
+
+    public MarvelCharacter getCharacterDetails(String characterId) 
+    throws IOException{
+        MarvelCharacter dd = null;
+        String charDetailsJson = (String)redisTemplate.opsForValue().get(characterId);
+        if(charDetailsJson != null){
+            dd = MarvelCharacter.createFromCache(charDetailsJson);
+        }else{
+            Optional<MarvelCharacter> op = 
+                    marvelApisvc
+                    .getCharacterDetails(charDetailsJson);
+
+            dd = op.get();
+            redisTemplate.opsForValue()
+                        .set(characterId, dd.toJSON().toString());
+            long currentDateTime = Instant.now().getMillis();
+            Date afterAdding60Mins = new Date(currentDateTime 
+                    + ( 60 * 60 * 1000));
+            redisTemplate.expireAt(characterId, afterAdding60Mins);
+        }
+        return dd;
+    }
+
+    public Comment insertComment(Comment c){
+        return this.charCommentRepo.insertComment(c);
+    }
+
+    public List<Comment> getAllComments(String charId){
+        return this.charCommentRepo.getAllComment(charId);
+    }
 }
