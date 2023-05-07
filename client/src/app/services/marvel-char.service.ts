f8b876c764587b4c95c1bf8d278ac8b45f5fbd5f
kenken64 <bunnyppl@hotmail.com>
2023-05-07 13:31:00 +0800

    upd

--- client/src/app/services/marvel-char.service.ts
+++ client/src/app/services/marvel-char.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, firstValueFrom, lastValueFrom } from 'rxjs';
+import { Observable, catchError, firstValueFrom, lastValueFrom, of, tap } from 'rxjs';
 import { Character } from '../model/Character';
 import { Comment } from '../model/Comment';
 
@@ -32,14 +32,30 @@ export class MarvelCharService {
   }
 
  
-  saveComment(c:Comment) : Observable<any>{
+  saveComment(c:Comment) : Observable<Comment>{
     const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
     const body=JSON.stringify(c);
     console.log("save comment !");
     console.log("save comment !" + c.charId);
     return this.httpClient
       .post<Comment>(this.MARVEL_API_URL+"/" + c.charId, body, 
-      {headers: headers});
+      {headers: headers}).pipe(
+        tap(_ => console.log(`add comment`)),
+      catchError(this.handleError<Comment>('saveComment')));
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      return of(result as T);
+    };
   }
 
   getCharComments(charId: string): Promise<Comment[]> {
